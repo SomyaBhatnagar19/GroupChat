cf0ce6c8a9aa8b6abd90cfced65aec746ef240b4
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2024-04-19 22:38:36 +0530

    Authorization middleware created.

---
+++ server/middleware/auth.js
@@ -0,0 +1,20 @@
+/* /server/middleware/auth.js */
+
+const jwt = require("jsonwebtoken");
+const User = require("../model/userModel");
+
+const authenticate = (req, res, next) => {
+  try {
+    const token = req.header("Authorization");
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+    User.findByPk(user.userId).then((user) => {
+      req.user = user;
+      next();
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(401).json({ success: false });
+  }
+};
+
+module.exports = authenticate;
